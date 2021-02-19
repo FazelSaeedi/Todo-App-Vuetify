ce8de7dcad4f973a12f2904c6d7984fc9087640d
FazelSaeedi <FazelSaeedi@iran.ir>
2021-02-19 04:20:02 +0330

    add item to nav Draw

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer
-        v-model="drawer"
-        app
-    >
-      <!--  -->
+    v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Vutify Todo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            :)) Smile !
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+          dense
+          nav
+      >
+        <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -21,6 +49,11 @@
 
 <script>
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({ drawer: null ,
+    items: [
+      { title: 'Todo', icon: 'mdi-view-dashboard' },
+      { title: 'About', icon: 'mdi-help-box' },
+    ],
+  }),
 }
 </script>
